f7b23deab19b2b7fda1dbe72e2f6ebf0ae0f7e07
GopalSaraf <sarafgopal37@gmail.com>
2024-06-18 14:22:49 +0530

    Added doc link

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ACB Nashik Website
+
+### Documentation on [Postman](https://documenter.getpostman.com/view/30434267/2sA3XSBMLu)
+
+`https://documenter.getpostman.com/view/30434267/2sA3XSBMLu`
--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,5 +1,9 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  return NextResponse.json({ message: "Hello World" });
+  return NextResponse.json({
+    message: "ACB Nashik Website API",
+    documentation: "https://documenter.getpostman.com/view/30434267/2sA3XSBMLu",
+    note: "This is a temporary documentation, will be updated as the project progresses",
+  });
 }
